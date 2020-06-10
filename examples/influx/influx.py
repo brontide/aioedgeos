df9860f43c82a9e9c80e52543d4e21ed5a282d14
Eric Warnke <ericew@gmail.com>
2020-06-10 07:08:51 -0400

    Sanity and debugging

--- examples/influx/influx.py
+++ examples/influx/influx.py
@@ -12,7 +12,7 @@ import os
 ROUTER_USERNAME = os.environ['ROUTER_USERNAME']
 ROUTER_PASSWORD = os.environ['ROUTER_PASSWORD']
 ROUTER_URL      = os.environ['ROUTER_URL']
-ROUTER_SSL      = os.environ.get('ROUTER_SSL', True) # Default to enforcing ssl
+ROUTER_SSL      = os.environ.get('ROUTER_SSL', 'f'*64).lower() # Default to enforcing ssl
 INFLUX_HOST     = os.environ['INFLUX_HOST']
 INFLUX_PORT     = os.environ.get('INFLUX_PORT', 8086)
 INFLUX_DB       = os.environ['INFLUX_DB']
@@ -30,8 +30,12 @@ ssl_check = True
 if isinstance(ROUTER_SSL, str) and len(ROUTER_SSL) == 64:
     # presume this is a fingerprint
     ssl_check = aiohttp.Fingerprint(a2b_hex(ROUTER_SSL))
-elif ROUTER_SSL in [ 'no', 'false', 'NO', 'FALSE', 'False' ]:
-    ssl_cehck = False
+elif ROUTER_SSL in [ 'no', 'false']:
+    ssl_check = False
+elif ROUTER_SSL in [ 'yes', 'true']:
+    ssl_check = True
+else:
+    raise Exception(f"ROUTER_SSL {ROUTER_SSL} is invalid")
 
 influx_auth = {}
 if INFLUX_USERNAME or INFLUX_PASSWORD:
@@ -236,5 +240,25 @@ async def main_loop():
     except aiohttp.client_exceptions.ServerFingerprintMismatch as e:
         fphash = b2a_hex(e.got)
         print(f"Server replied with different fingerprint hash of {fphash}, if this is expected please update config")
+
+print(f'''
+================================================
+ROUTER_USERNAME = {ROUTER_USERNAME}
+ROUTER_PASSWORD = **HIDDEN**
+ROUTER_URL      = {ROUTER_URL}
+ROUTER_SSL      = {ROUTER_SSL}
+ - ssl_check   -> {ssl_check}
+INFLUX_HOST     = {INFLUX_HOST}
+INFLUX_PORT     = {INFLUX_PORT}
+INFLUX_DB       = {INFLUX_DB}
+INFLUX_USERNAME = {INFLUX_USERNAME}
+INFLUX_PASSWORD = **HIDDEN**
+PING_TARGET     = {PING_TARGET}
+PING_COUNT      = {PING_COUNT}
+PING_SIZE       = {PING_SIZE}
+PING_INTERVAL   = {PING_INTERVAL}
+================================================
+Starting main loop
+''')
 
 asyncio.run(main_loop())
